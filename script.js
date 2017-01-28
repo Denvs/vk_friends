4a95d7e2c8ffe2a02d9c09dd6a36dfd203dd4006
denv <denv@example.com>
2017-01-28 20:09:20 +0200

    starting getting friends list 2

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ document.addEventListener("DOMContentLoaded",
                   console.log(response.session.user);
                   var user_first_name = response.session.user.first_name;
                   document.querySelector("#greeting").textContent = "Hello, " + user_first_name;
-                  var friends = response.session.user.friends.get;
+                  var user_id = response.session.user.id;
+                  var friends = user_id.friends.get;
                   console.log(friends);
                 } else {
                 // Неавторизованный в Open API пользователь,  response.status="not_authorized" 
