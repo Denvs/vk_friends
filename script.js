3d5709f8523f8d3c4316f14a5b820ecade8bab2e
denv <denv@example.com>
2017-01-29 01:03:32 +0200

    getting friends list

--- script.js
+++ script.js
@@ -64,7 +64,6 @@ document.addEventListener("DOMContentLoaded",
               if(response) {
                   friends = response.response;
                   for (var f in friends) {
-                      console.log(friends[f]);
                       VK.Api.call('users.get', {user_id: friends[f]}, function(response) {
                           if(response) {
                               console.log(response.response[0]);
